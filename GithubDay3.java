952d0902ad204f85d63f2e4d027581209a41967d
avanin003 <avanin003@gmail.com>
2018-11-29 08:46:01 -0500

    new lines added
    testing merge problems

--- GithubDay3.java
+++ GithubDay3.java
@@ -2,6 +2,7 @@ public class GithubDay3{
 	public static void main(String[] args){
 
 		System.out.println("Venkata");
-		System.out.println("Avani");
+		System.out.println("Avani is so cool");
+		System.out.println("Testing merge problem");
 	}
 }
